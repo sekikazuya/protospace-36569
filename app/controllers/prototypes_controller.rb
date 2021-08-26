6f42c1a3685bb8d4fab616c424403a90c5bbf160
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-08-26 17:00:10 +0900

    アクションの指定方法の変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update]
   before_action :set_prototype, only: [:show, :edit, :update]
 
   def index
